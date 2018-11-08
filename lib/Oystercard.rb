d2c11c555663429c1b238dca466708d866849459
Stuart Adair <stu@silverka.co.uk>
2018-11-08 14:29:55 +0000

    Test that git is working

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -1,21 +1,21 @@
 class Oystercard
-attr_reader :balance
-MAX_VALUE = 90
+  attr_reader :balance
+  MAX_VALUE = 90
 
-def initialize
-  @balance = 0
+  def initialize
+    @balance = 0
+  end
 
-end
-def topup(money)
-  raise'maximum value exceeded' if @balance + money > MAX_VALUE
-  @balance += money
-end
+  def topup(money)
+    raise'maximum value exceeded' if @balance + money > MAX_VALUE
+    @balance += money
+  end
 
-def deduct(money)
-  @balance -= money
-end
+  def deduct(money)
+    @balance -= money
+  end
 
-def in_journey?
-  @in_journey
-end
+  def in_journey?
+    @in_journey
+  end
 end
